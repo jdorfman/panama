0818b77b6095e1148f083e74e6eca4f9320e4b60
Justin Dorfman <justindorfman@gmail.com>
2014-07-22 02:37:46 -0700

    check point, tired

---
+++ server.js
@@ -0,0 +1,19 @@
+var five = require('johnny-five'),
+    board = new five.Board(),
+	Blinker = require('./lib/blinker'),
+	MaxCdnitiator = require('./lib/maxcdnitiator');
+	// BrowserWars = require('./lib/browserwarzzzzzz');
+
+board.on('ready', function() {
+
+	var blinker = new Blinker(five),
+		maxcdn = new MaxCdnitiator({dataUrl: '/v3/reporting/logs.json?status=304', eventName: '304'});
+
+	maxcdn.on('304', function(data) {
+		console.log(data.user_agent);
+
+		// Instead of blinker, place browser-wars here:
+		// blinker.blink(data);
+	});
+
+});
