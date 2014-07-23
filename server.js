ef966aeaded3e6eff876ae32603a4629af7ef0de
Justin Dorfman <justindorfman@gmail.com>
2014-07-23 12:36:24 -0700

    browser wars

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 var five = require('johnny-five'),
     board = new five.Board(),
 	Blinker = require('./lib/blinker'),
-	MaxCdnitiator = require('./lib/maxcdnitiator');
-	// BrowserWars = require('./lib/browserwarzzzzzz');
+	MaxCdnitiator = require('./lib/maxcdnitiator'),
+	BrowserWars = require('./lib/browserwars');
 
 board.on('ready', function() {
 
-	var blinker = new Blinker(five),
-		maxcdn = new MaxCdnitiator({dataUrl: '/v3/reporting/logs.json?status=304', eventName: '304'});
+	var blinker = new Blinker(five);
+	// 	maxcdn = new MaxCdnitiator({dataUrl: '/v3/reporting/logs.json?status=304', eventName: '304'});
+	
+	var browserwars = new BrowserWars(five),
+	  	maxcdn = new MaxCdnitiator({dataUrl: '/v3/reporting/logs.json?status=304', eventName: '304'});
 
 	maxcdn.on('304', function(data) {
-		//console.log(data.user_agent);
-
-		// Instead of blinker, place browser-wars here:
-		blinker.blink(data);
+		//var ua = data.user_agent;
+		console.log(data.user_agent);
+		browserwars.blink(data);
 	});
 });
